031eb4c36d1c97cdd1bd4ff3be11c2d94a027b57
Shivkarmaholic <kharadkarsandeep1@gmail.com>
2022-07-17 15:11:13 +0530

    Add project files.

---
+++ Ass_MVCFoodController/Controllers/FoodController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+namespace Ass_MVCFoodController.Controllers
+{
+    public class FoodController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(IFormCollection form,ICollection<string> chksouth, ICollection<string> chknorth)
+        {
+            ViewBag.Name = form["txtname"];
+            ViewBag.Mobile = form["txtmobile"];
+            ViewBag.South= chksouth;
+            ViewBag.North= chknorth;
+
+            return View("Details");
+        }
+        public IActionResult Details()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Ass_MVCFoodController/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ass_MVCFoodController.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
